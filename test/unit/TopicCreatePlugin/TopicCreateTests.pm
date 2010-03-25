4acc05155b3a146917dc6fcc982c6a08611bb996
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-03-25 14:51:12 +0000

    Item8765: Replace use base with use; our @ISA;
    git-svn-id: http://svn.foswiki.org/trunk/TopicCreatePlugin@6912 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- test/unit/TopicCreatePlugin/TopicCreateTests.pm
+++ test/unit/TopicCreatePlugin/TopicCreateTests.pm
@@ -3,7 +3,8 @@
 #   - recursive %TOPICCREATE%'s
 package TopicCreateTests;
 
-use base qw( FoswikiFnTestCase );
+use FoswikiFnTestCase;
+our @ISA = qw( FoswikiFnTestCase );
 
 use strict;
 use Foswiki;
