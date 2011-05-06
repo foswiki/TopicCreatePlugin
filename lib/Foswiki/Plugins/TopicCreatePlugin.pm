a6adad0e5c73edf58afcfa565c53784e2d33f103
AndrewJones <AndrewJones@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2011-05-06 10:57:33 +0000

    Item8480: bump version
    git-svn-id: http://svn.foswiki.org/trunk/TopicCreatePlugin@11639 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/TopicCreatePlugin.pm
+++ lib/Foswiki/Plugins/TopicCreatePlugin.pm
@@ -25,7 +25,7 @@ use vars qw(
 );
 
 $VERSION = '$Rev$';
-$RELEASE = '1.4';
+$RELEASE = '1.5';
 $SHORTDESCRIPTION =
   'Automatically create a set of topics and attachments at topic save time';
 $NO_PREFS_IN_TOPIC = 1;
